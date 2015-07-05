870e6e029ea591afe8544d10dd61627e30adc380
zjh <501508468@qq.com>
2015-07-06 07:06:57 +0800

    add list.h

---
+++ list.h
@@ -0,0 +1,21 @@
+#ifndef _List_H
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+List MakeEmpty( List L );
+int IsEmpty( List L );
+int IsLast( Position P, List t );
+Position Find( Elem X, List L );
+void Delete( Elem X, List L );
+Position FindPrevious( Elem X, List L );
+void Insert( Elem X, List L, Position P );
+void DeleteList( List L );
+Position Header( List L );
+Position First( List L );
+Position Advance( Postion P );
+Elem Retrieve( Position P );
+
+#endif
